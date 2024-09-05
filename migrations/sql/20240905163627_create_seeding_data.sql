16eb9d87280c0bbd4bec8ce3dfad28485a4dbdbd
hilmiikhsan <ikhsanhilmimuhammad@gmail.com>
2024-09-06 00:43:11 +0700

    feat: add migration file

---
+++ migrations/sql/20240905163627_create_seeding_data.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT INTO cart_items (user_id, product_id, qty)
+VALUES
+    ('a4b7a3f1-751a-4a10-b506-99202581427b', '550e8400-e29b-41d4-a716-446655440001', 2),
+    ('a4b7a3f1-751a-4a10-b506-99202581427b', '550e8400-e29b-41d4-a716-446655440002', 1),
+    ('a4b7a3f1-751a-4a10-b506-99202581427b', '550e8400-e29b-41d4-a716-446655440003', 3),
+    ('c396f23e-a097-476d-aae5-cfc9973634f3', '550e8400-e29b-41d4-a716-446655440005', 4),
+    ('c396f23e-a097-476d-aae5-cfc9973634f3', '550e8400-e29b-41d4-a716-446655440006', 5);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+-- SELECT 'down SQL query';
+-- +goose StatementEnd
